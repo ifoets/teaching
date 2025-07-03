16ab63a7dfa700d1cdcd68ca9301c9be1ba21da3
ifoets <ifoets@gmail.com>
2025-07-03 12:57:31 +0530

    Leet Code  algo two pointer technique

---
+++ algorithms/src/main/java/com/leetcode/algorithms/two/pointer/medium/ITwoPointerMedium.java
@@ -0,0 +1,16 @@
+package com.leetcode.algorithms.two.pointer.medium;
+
+import java.util.List;
+
+public interface ITwoPointerMedium {
+
+    /**5. Longest Palindromic Substring*/
+     String longestPalindrome(String s);
+     int expandAroundCenter(String s, int l, int r);
+
+     /**11. Container With Most Water*/
+     int maxArea(int[] h);
+
+     /**15. 3Sum*/
+     List<List<Integer>> threeSum(int[] nums);
+}
