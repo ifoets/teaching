bff540e78619ad39506130711379d044a4935709
ifoets <ifoets@gmail.com>
2025-07-05 21:36:42 +0530

    live-interview file location change

---
+++ interview-corner/src/main/java/interview/preparation/company/interviews/question/ITesco.java
@@ -0,0 +1,7 @@
+package interview.preparation.company.interviews.question;
+
+public interface ITesco {
+
+    /**Get Max price to buy and shell stocks in different days*/
+     int getMaxGainInStock(int[] s);
+}
