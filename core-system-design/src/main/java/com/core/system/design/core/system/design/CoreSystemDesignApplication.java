b5112d78f06da52cfe7bcea4c22495200c04c8a3
ifoets <ifoets@gmail.com>
2025-08-13 20:06:53 +0530

    core-system-design module

---
+++ core-system-design/src/main/java/com/core/system/design/core/system/design/CoreSystemDesignApplication.java
@@ -0,0 +1,13 @@
+package com.core.system.design.core.system.design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreSystemDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreSystemDesignApplication.class, args);
+	}
+
+}
